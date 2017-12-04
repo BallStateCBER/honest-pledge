f66b55ec64dbfe51a41438a18d533a1cf31275cc
Graham Watson <graham@phantomwatson.com>
2017-12-04 17:20:48 -0500

    Wrap wrapper in normal HTML page structure

--- src/Templates/wrapper.php
+++ src/Templates/wrapper.php
@@ -1,12 +1,22 @@
-<h1>
-    The Honest Muncie Pledge
-</h1>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-15" />
+        <style>
+        </style>
+    </head>
+    <body>
+        <h1>
+            The Honest Muncie Pledge
+        </h1>
 
-[pledge]
+        [pledge]
 
-<p id="signature">
-    [name]
-</p>
-<p id="date">
-    [date]
-</p>
+        <p id="signature">
+            [name]
+        </p>
+        <p id="date">
+            [date]
+        </p>
+
+    </body>
+</html>
